3232964c377a5a4d9fc7291e12205dc8a5499059
jessica <jragland1@live.maryville.edu>
2018-04-17 10:43:26 -0400

    Initial Commit

---
+++ Florence Study Abroad App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Florence Study Abroad App
+//
+//  Created by user134267 on 4/17/18.
+//  Copyright © 2018 user134267. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
